ef587d78a05af1baf728d65a83e0e08456af8a09
Pau Penin <paupenin@gmail.com>
2016-08-02 20:33:18 +0200

    Factorize up to 10

--- spec/PrimeFactorsSpec.php
+++ spec/PrimeFactorsSpec.php
@@ -43,9 +43,29 @@ class PrimeFactorsSpec extends ObjectBehavior
         $this->factorize(5)->shouldReturn([5]);
     }
 
-    function it_factorizes_6_as_6()
+    function it_factorizes_6_as_2_3()
     {
         $this->factorize(6)->shouldReturn([2,3]);
+    }
+
+    function it_factorizes_7_as_7()
+    {
+        $this->factorize(7)->shouldReturn([7]);
+    }
+
+    function it_factorizes_8_as_2_2_2()
+    {
+        $this->factorize(8)->shouldReturn([2,2,2]);
+    }
+
+    function it_factorizes_9_as_3_3()
+    {
+        $this->factorize(9)->shouldReturn([3,3]);
+    }
+
+    function it_factorizes_10_as_2_5()
+    {
+        $this->factorize(10)->shouldReturn([2,5]);
     }
 
 
