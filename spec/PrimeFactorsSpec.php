405a607e2e362075f05e4db141408891e50472ca
Pau Penin <paupenin@gmail.com>
2016-08-02 20:07:00 +0200

    Factorize up to 3

--- spec/PrimeFactorsSpec.php
+++ spec/PrimeFactorsSpec.php
@@ -7,8 +7,30 @@ use PhpSpec\ObjectBehavior;
 
 class PrimeFactorsSpec extends ObjectBehavior
 {
+    /**
+     * initializable test
+     */
     function it_is_initializable()
     {
         $this->shouldHaveType(PrimeFactors::class);
     }
+
+    /**
+     * factorization tests
+     */
+    function it_factorizes_1_as_empty()
+    {
+        $this->factorize(1)->shouldReturn([]);
+    }
+
+    function it_factorizes_2_as_2()
+    {
+        $this->factorize(2)->shouldReturn([2]);
+    }
+
+    function it_factorizes_3_as_3()
+    {
+        $this->factorize(3)->shouldReturn([3]);
+    }
+
 }
--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -2,4 +2,17 @@
 
 class PrimeFactors
 {
+    /**
+     * Returns all factors for the given number
+     * @param  integer $num
+     * @return array
+     */
+    public function factorize($num)
+    {
+        if($num % 3 == 0) return [3];
+
+        if($num % 2 == 0) return [2];
+        
+        return [];
+    }
 }
